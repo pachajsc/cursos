abf9acdf1754012364543fcd8b45afcd44b3a929
jose <pachajsc@gmail.com>
2020-09-19 13:57:30 -0300

    first commit

---
+++ src/components/CardItems.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CardItems = () => {
+  return (
+    <></>
+  );
+}
+
+export default CardItems;
---
+++ src/components/CollapseCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CollapseCard = () => {
+  return (
+    <></>
+  );
+}
+
+export default CollapseCard;
